afb1a62f324c8353d7556b818d6a121b7886ec7f
Adrian C <adrian@iMac-de-imac4-2.local>
2017-11-06 17:31:25 -0300

    Agregando reports.vue

---
+++ src/components/Reports.vue
@@ -0,0 +1,48 @@
+<template>
+        <div id='kibana' class="" >
+        <iframe :src="url" height="1100" width="100%"></iframe>
+      </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import axios from 'axios';
+import config from '../../config/appconfig.js';
+
+export default {
+  name: 'hello',
+  data() {
+      return {
+        url:config.url_reports
+      }
+    },
+  methods: {
+    
+  },
+  created() {
+
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
